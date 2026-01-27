557d5083b4d70538d33196f8517c52821c6dbf16
vafivafi <vladvaskin7@gmail.com>
2026-01-27 16:11:15 +0300

    add: добавил логирование в main

--- src/app/main.py
+++ src/app/main.py
@@ -3,14 +3,16 @@ from contextlib import asynccontextmanager
 
 from src.infrastructure.db.session import DataBaseConfig
 from src.infrastructure.db.models.admin import AdminsOrm
+from src.infrastructure.log.logger import logger
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await DataBaseConfig.connect()
-    
+    logger.info("База данных поключена")
     yield
     
     await DataBaseConfig.disconnect()
+    logger.info("База данных отключена")
     
 app = FastAPI(lifespan=lifespan)
