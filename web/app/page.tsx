8f2a6c333f6e4ba9d7f7af906f24ac1b24c28955
CodesForge <codesforge@icloud.com>
2026-01-28 14:32:49 +0300

    edit: web/app/page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,12 +6,17 @@ import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/ca
 import { Server, Boxes, PawPrint, CalendarCheck, CircleCheck, Ban, Leaf, ArrowRight, HeartPulse } from "lucide-react"
 import { useState } from "react"
 
+import dog6 from "@/images/dog6.png"
+import cat3 from "@/images/cat3.png"
+import dog5 from "@/images/dog5.png"
+import dog4 from "@/images/dog4.png"
 import cat2 from "@/images/cat2.png"
 import cat from "@/images/cat.png"
 import dog2 from "@/images/dog2.png"
 import dog from "@/images/dog.png"
 import dog3 from "@/images/dog3.png"
 import Image from "next/image"
+import { Input } from "@/components/ui/input"
 
 export default function Page() {
    return(
@@ -160,6 +165,129 @@ export default function Page() {
             </Card>
             <div className="w-full bg-[#FDC7E5] max-w-sm flex justify-end items-center rounded-[10px]">
                 <Image src={cat2} className="h-50 w-150 flex" alt="cat.png" />
+            </div>
+        </div>
+        <div className="w-full flex justify-center gap-5 px-63">
+            <div className="w-full bg-[#FDC7E5] max-w-sm flex justify-end items-center rounded-[10px]">
+                <Image src={dog4} className="h-50 w-150 flex" alt="cat.png" />
+            </div>
+            <Card className="w-full bg-[#F5F5F5]">
+                <CardHeader>
+                    <CardTitle className="text-3xl font-light mt-5">Яблоки и тыква</CardTitle>
+                    <CardDescription className="mt-8">Содержат микроэлементы и клетчатку, которая поддерживает работу пищеварительной системы и способствует усвоению питательных веществ.</CardDescription>
+                </CardHeader>
+            </Card>
+        </div>
+        <div className="w-full flex justify-center gap-5 px-63">
+            <Card className="w-full bg-[#F5F5F5]">
+                <CardHeader>
+                    <CardTitle className="text-3xl font-light mt-5">Пробиотики и пребиотики</CardTitle>
+                    <CardDescription className="mt-8">Способствуют формированию и поддержанию естественной микрофлоры кишечника и комфортного пищеварения.</CardDescription>
+                </CardHeader>
+            </Card>
+            <div className="w-full bg-[#FDC7E5] max-w-sm flex justify-end items-center rounded-[10px]">
+                <Image src={dog5} className="h-50 w-150 flex" alt="cat.png" />
+            </div>
+        </div>
+        <div className="w-full flex justify-center gap-5 px-63">
+            <div className="w-full bg-[#FDC7E5] max-w-sm flex justify-end items-center rounded-[10px]">
+                <Image src={cat3} className="h-70 w-150 flex" alt="cat.png" />
+            </div>
+            <div className="flex flex-col gap-3 w-full">
+                <Card className="w-full bg-[#F5F5F5]">
+                    <CardHeader>
+                        <CardTitle className="text-3xl font-light mt-5">Экстракт юкки шидигера</CardTitle>
+                        <CardDescription className="mt-8">Используется для поддержания нормального пищеварения и уменьшения неприятных запахов.</CardDescription>
+                    </CardHeader>
+                </Card>
+                <Card className="w-full bg-[#F5F5F5]">
+                    <CardHeader>
+                        <CardTitle className="text-3xl font-light mt-5">Глюкозамин и хондроитин</CardTitle>
+                        <CardDescription className="mt-8">Используются для поддержания нормального состояния суставов и хрящевой ткани.</CardDescription>
+                    </CardHeader>
+                </Card>
+            </div>
+        </div>
+        <div className="w-full flex flex-col items-center justify-center text-center mt-8">
+            <div className="max-w-3xl">
+                <p className="text-4xl font-normal">
+                Часто задаваемые вопросы
+                </p>
+            </div>
+            <CardDescription className=" mt-3 max-w-3xl text-[20px]">
+                Собрали ответы на вопросы, которые чаще всего возникают при выборе корма WellWet.
+            </CardDescription>
+            <Card className="mt-8 w-full bg-[#F5F5F5] max-w-6xl">
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        <CardTitle className="text-2xl">Что значит holistic-корм?</CardTitle>
+                        <div className="bg-[#C5E8EE] p-2 rounded-[100%] border border-[#06B2D3]">
+                            <PawPrint className="text-[#06B2D3]"/>
+                        </div>
+                    </div>
+                    <CardDescription className="text-left mt-3">Holistic-корм — это питание, разработанное с учётом естественных потребностей питомца. В таких кормах используется качественный белок, сбалансированный состав и исключаются лишние ингредиенты, не несущие питательной ценности.</CardDescription>
+                </CardHeader>
+            </Card>
+            <Card className="mt-8 w-full bg-[#F5F5F5] max-w-6xl">
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        <CardTitle className="text-2xl">Что значит holistic-корм?</CardTitle>
+                        <div className="bg-[#C5E8EE] p-2 rounded-[100%] border border-[#06B2D3]">
+                            <PawPrint className="text-[#06B2D3]"/>
+                        </div>
+                    </div>
+                    <CardDescription className="text-left mt-3">Holistic-корм — это питание, разработанное с учётом естественных потребностей питомца. В таких кормах используется качественный белок, сбалансированный состав и исключаются лишние ингредиенты, не несущие питательной ценности.</CardDescription>
+                </CardHeader>
+            </Card>
+            <Card className="mt-8 w-full bg-[#F5F5F5] max-w-6xl">
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        <CardTitle className="text-2xl">Что значит holistic-корм?</CardTitle>
+                        <div className="bg-[#C5E8EE] p-2 rounded-[100%] border border-[#06B2D3]">
+                            <PawPrint className="text-[#06B2D3]"/>
+                        </div>
+                    </div>
+                    <CardDescription className="text-left mt-3">Holistic-корм — это питание, разработанное с учётом естественных потребностей питомца. В таких кормах используется качественный белок, сбалансированный состав и исключаются лишние ингредиенты, не несущие питательной ценности.</CardDescription>
+                </CardHeader>
+            </Card>
+            <Card className="mt-8 w-full bg-[#F5F5F5] max-w-6xl">
+                <CardHeader>
+                    <div className="flex flex-row justify-between">
+                        <CardTitle className="text-2xl">Что значит holistic-корм?</CardTitle>
+                        <div className="bg-[#C5E8EE] p-2 rounded-[100%] border border-[#06B2D3]">
+                            <PawPrint className="text-[#06B2D3]"/>
+                        </div>
+                    </div>
+                    <CardDescription className="text-left mt-3">Holistic-корм — это питание, разработанное с учётом естественных потребностей питомца. В таких кормах используется качественный белок, сбалансированный состав и исключаются лишние ингредиенты, не несущие питательной ценности.</CardDescription>
+                </CardHeader>
+            </Card>
+        </div>
+        <div className="w-full flex flex-col items-center justify-center text-center mt-8">
+            <div className="flex flex-row gap-3 w-full max-w-6xl">
+                <div className="bg-[#FDCDE9] p-2 rounded-[10px]">
+                    <Image src={dog6} className="h-110 w-115 flex" alt="cat.png" />
+                </div>
+                <Card className="w-full">
+                    <CardHeader>
+                        <CardTitle className="text-3xl text-left">Заявка на сотрудничество</CardTitle>
+                        <div className="max-w-xl">
+                            <CardDescription className="text-[20px] text-left mt-3">Заполните форму и мы вышлем все материалы на вашу электронную почту</CardDescription>
+                        </div>
+                        <div className="flex flex-row gap-4 mt-3">
+                            <Input placeholder="Ваше имя" className="bg-[#F2F2F2] p-6"></Input>
+                            <Input placeholder="Email или телефон" className="bg-[#F2F2F2] p-6"></Input>
+                        </div>
+                        <div className="mt-4">
+                            <Input placeholder="Тип организации" className="bg-[#F2F2F2] p-6"></Input>
+                        </div>
+                        <div className="mt-4">
+                            <Input placeholder="Ваш вопрос или комментарий" className="bg-[#F2F2F2] p-6"></Input>
+                        </div>
+                        <div className="flex items-start">
+                            <Button className="p-7 mt-8 bg-[#FDD5E9] text-black font-normal">Отправить<div className="bg-white rounded-[5px] w-8 h-8 flex items-center justify-center ml-3"><ArrowRight style={{ strokeWidth: 3 }} className="text-[#F462AB]"/></div></Button>
+                        </div>
+                    </CardHeader>
+                </Card>
             </div>
         </div>
     </div>
